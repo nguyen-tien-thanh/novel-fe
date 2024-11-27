e0462ddb5323300d11a8236134683f866cab7c42
SonNguyenne <ts29032001@gmail.com>
2024-11-27 11:31:07 +0700

    chore
    comment middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,14 @@ import { NextResponse } from 'next/server'
 import { ROLE } from './types'
 
 export default auth(req => {
-  const { nextUrl, auth } = req
+  // const { nextUrl, auth } = req
 
-  const user = auth?.user
-  const isAdminPage = nextUrl?.pathname.startsWith('/admin')
+  // const user = auth?.user
+  // const isAdminPage = nextUrl?.pathname.startsWith('/admin')
 
-  if (isAdminPage && !user?.role.includes(ROLE.ADMIN)) {
-    return Response.redirect(new URL('/error', nextUrl))
-  }
+  // if (isAdminPage && !user?.role.includes(ROLE.ADMIN)) {
+  //   return Response.redirect(new URL('/error', nextUrl))
+  // }
 
   return NextResponse.next()
 })
